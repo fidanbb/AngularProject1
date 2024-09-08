83fde896fbfb327f129e2ce6759518acca5113ff
fidanbb <fidanbb@code.edu.az>
2024-09-08 19:59:14 +0400

    changing the status

--- src/app/components/md-form/md-form.component.ts
+++ src/app/components/md-form/md-form.component.ts
@@ -20,8 +20,24 @@ import { log } from 'console';
   </form>
 <br>
 <br>
-  <button (click)="ok()">Ok</button>
-  Valid:{{frm.valid}}
+  <button (click)="ok()">Ok</button> <br>
+  <button (click)="markAsTouched()">markAsTouched</button> <br>
+  <button (click)="markAllAsTouched()">markAllAsTouched</button> <br>
+  <button (click)="markAsUntouched()">markAsUntouched</button> <br>
+  <button (click)="markAsDirty()">markAsDirty</button> <br>
+  <button (click)="markAsPristine()">markAsPristine</button> <br>
+  <button (click)="disable()">disable</button> <br>
+  <button (click)="enable()">enable</button> <br>
+
+  Valid:{{frm.valid}} <br>
+  Form Touched:{{frm.touched}} <br>
+  Name Touched:{{frm.get("name").touched}} <br>
+  Address Touched:{{frm.get("address").touched}} <br>
+  Country Touched:{{frm.get("address").get("country").touched}} <br>
+  Form Dirty:{{frm.dirty}} <br>
+  Name Touched:{{frm.get("name").dirty}} <br>
+  Form Pristine:{{frm.pristine}} <br>
+  Name Pristine:{{frm.get("name").pristine}} <br>
   `,
   styleUrl: './md-form.component.scss'
 })
@@ -55,6 +71,34 @@ this.frm=formBuilder.group({
   }
   ok(){
     this.frm.get("name").setValue("Fidan",{onlySelf:true})
+  }
+  markAsTouched(){
+    this.frm.markAsTouched();
+    this.frm.get("name").markAsTouched({onlySelf:true});
+  }
+  markAllAsTouched(){
+    //this.frm.markAllAsTouched();
+    this.frm.get("address").markAllAsTouched();
+  }
+  markAsUntouched(){
+    this.frm.markAsUntouched();
+
+  }
+  markAsDirty(){
+    this.frm.markAsDirty();
+
+  }
+  markAsPristine(){
+    this.frm.markAsPristine();
+
+  }
+  disable(){
+    this.frm.get("name").disable();
+
+  }
+  enable(){
+    this.frm.get("name").enable();
+
   }
   onSubmit(){
     console.log(this.frm.value);
